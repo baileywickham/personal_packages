99367c54cfbfd0c3bd26536a19bc2c4374ff629d
baileywickham <baileywickham@gmail.com>
2018-01-16 08:48:40 -0800

    init

---
+++ personal_packages.sh
@@ -0,0 +1,17 @@
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+
+apt update
+apt install gpp python3-pip spotify-client sublime-text discord vim git vlc htop -y
+pip3 install pipenv
+echo "dont forget pia"
