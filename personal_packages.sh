9c001aa725909f2c5e5b27d84b6cb2e4e5339c1b
Bailey Wickham <baileywickham@gmail.com>
2018-03-16 12:38:37 -0700

    added pipenv

--- personal_packages.sh
+++ personal_packages.sh
@@ -11,13 +11,16 @@ packages () {
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 
 	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+	
+	apt install software-properties-common python-software-properties -y
+	add-apt-repository ppa:pypa/ppa
 }
 
 main() {
 	packages()
 
 	apt update
-	apt install gpp python3-pip spotify-client sublime-text discord vim git vlc htop -y
+	apt install gpp python3-pip spotify-client sublime-text discord vim git vlc htop pipenv -y
 	pip3 install pipenv
 
 	echo "dont forget pia"
