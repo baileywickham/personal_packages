6a84cdd6364aaee32e98521c7abdb7264c0c1b60
baileywickham <baileywickham@gmail.com>
2018-12-05 12:41:56 -0800

    yikes' ' q

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def copyFile(*, file, path):
     file = Path(file)
     shutil.copy2(file,path)
 
-def linkFile(file): 
-    subprocess.Popen(['ln',os.getcwd()+f'/{file}',str(Path.home())+f'/{file}'])
+def linkFile(file, loc=str(Path.home())): 
+    subprocess.Popen(['ln','-sf',os.getcwd()+f'/{file}',loc+f'/{file}'])
 
 
 def main():
-    vim()
-    bash()
+    nvim()
+    #bash()
     tmux() 
     #ssh()
     linkFile('.gitignore_global')
@@ -28,27 +28,25 @@ def main():
         #copyFile(Path('70-utf.rules'),Path('/etc/udev/rules.d'/))
         #copyFile(Path('google-chrome.list',Path('/etc/apt/sources.list.d/google-chrome.list'))
     
-
-def vim():
-    if (home / Path('.vimrc')).exists():
-        os.remove(home / Path('.vimrc'))
-    linkFile('.vimrc')
-    subprocess.Popen(['git','clone','https://github.com/VundleVim/Vundle.vim.git', '~/.vim/bundle/Vundle.vim'])
-     #subprocess.Popen(['vim', '-c', 'PluginInstall'])
-
-
-def bash():
-    if (home / Path('.bashrc')).exists():
-        os.remove(home / Path('.bashrc'))
-    linkFile('.bashrc')
-
-
+def nvim():
+    pass
+   
 def tmux():
     if (home / Path('.vimrc')).exists() and (home / Path('.tmux.conf.local')).exists():
         os.remove(home / Path('.tmux.conf'))
         os.remove(home / Path('.tmux.conf.local'))
     linkFile('.tmux.conf')
     linkFile('.tmux.conf.local')
+
+def vim():
+    linkFile('.vimrc')
+    #subprocess.Popen(['git','clone','https://github.com/VundleVim/Vundle.vim.git', '~/.vim/bundle/Vundle.vim'])
+    #subprocess.Popen(['vim', '-c', 'PluginInstall'])
+
+def bash():
+    linkFile('.bashrc')
+
+
 
 def ssh():
     if not Path(home / '.ssh/config').exists():
