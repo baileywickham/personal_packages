46e30a5013ae3af7a4d57a910410acc343a43532
baileywickham <baileywickham@gmail.com>
2019-01-10 23:24:09 -0800

    added plug install

--- personal_packages.sh
+++ personal_packages.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 # This whole file is a really bad idea. DO NOT copy any of this code.
+vundle () {
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    }
 packages () {
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg |   apt-key add -
 
-	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub |   apt-key add -
-	echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' |   tee /etc/apt/sources.list.d/google-chrome.list
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub |   apt-key add -
+    echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' |   tee /etc/apt/sources.list.d/google-chrome.list
 
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-	echo deb http://repository.spotify.com stable non-free |   tee /etc/apt/sources.list.d/spotify.list
-	
-	apt install -y software-properties-common software-properties-common python3-dev python3-pip 
-	add-apt-repository ppa:pypa/ppa
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+    echo deb http://repository.spotify.com stable non-free |   tee /etc/apt/sources.list.d/spotify.list
+
+    apt install -y software-properties-common software-properties-common python3-dev python3-pip
+    add-apt-repository ppa:pypa/ppa
     apt update -qq
     apt install git  spotify-client google-chrome
 }
@@ -23,7 +29,7 @@ dir() {
 replace() {
     source ./declares.sh
     for i in ${!dotFile[@]}; do
-        echo "copying ${dotFile[i]}" 
+        echo "copying ${dotFile[i]}"
         yes | ln -sf "${PWD}/config/${dotFile[i]}" "${HOME}/${dotFile[i]}"
     done
     ln -sf "${PWD}/config/init.vim" "${HOME}/.config/nvim/"
@@ -31,12 +37,12 @@ replace() {
 
 
 addSSHLink() {
-	if [ ! -f  "~/.ssh/config" ]
-	then
-		ln -s $PWD/sshconfig ~/.ssh/config 
-	else 
-		echo "SSH Config already exists"
-	fi
+    if [ ! -f  "~/.ssh/config" ]
+    then
+        ln -s $PWD/sshconfig ~/.ssh/config
+    else
+        echo "SSH Config already exists"
+    fi
 }
 
 add2FA() {
