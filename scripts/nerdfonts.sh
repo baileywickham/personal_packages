c73541c3e83778216a72d7887eb55e77125257a4
baileywickham <baileywickham@gmail.com>
2020-03-01 17:03:18 -0800

    update nerdfont script

--- scripts/nerdfonts.sh
+++ scripts/nerdfonts.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # Incosada regualr
-if [ -d "~/.nerdfonts" ]; then
+if [ -d "~/.builds/nerdfonts" ]; then
     echo "nerdfonts directory already exists"
 else
-    git clone https://github.com/ryanoasis/nerd-fonts.git ~/.nerdfonts
-    ~/.nerdfonts/install.sh
+    git clone https://github.com/ryanoasis/nerd-fonts.git ~/.builds/nerdfonts
+    bash ~/.builds/nerdfonts/install.sh
 fi
