5b35397a1703d8bad70a953fd384fe5478320b20
baileywickham <baileywickham@gmail.com>
2020-10-10 10:54:56 -0700

    update modules

--- modules/install_zsh.sh
+++ modules/install_zsh.sh
@@ -8,7 +8,6 @@ function install_zsh() {
     if [ -d "$HOME/.oh-my-zsh" ]; then
         sub_sub "oh-my-zsh already installed"
     else
-        git submodule init
         sub_sub "linking oh-my-zsh"
         ln -s "${WORKDIR}/oh-my-zsh/" "${HOME}/.oh-my-zsh"
         #ln -s "${HOME}/.oh-my-zsh" "${PWD}/oh-my-zsh/"
